ee4a06a04d7463757c4540d5b4811f2ccf2ff3bb
p2008 <p2008@op.pl>
2018-12-15 15:28:49 +0100

    models

---
+++ twitter_app/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Tweet(models.Model):
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='tweets')
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_comments')
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE,
+                              related_name='tweet_commments')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    content = models.TextField()
+    user_from = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name='message_send')
+    user_to = models.ForeignKey(User, on_delete=models.CASCADE,
+                                related_name='message_received')
+    read = models.BooleanField()
+
+
